46c94044be35201a8e46214cbdeb0449496d4121
BadyBob <utkinalexey66@gmail.com>
2018-12-22 09:49:29 +0500

    Исправления

--- js/main.js
+++ js/main.js
@@ -52,6 +52,10 @@ var MAX_X = 1200;
 var MIN_Y = 130;
 var MAX_Y = 630;
 var COUNT_ADVERTS = 8;
+var MyPin = {
+  WIDTH: 50,
+  HEIGHT: 70
+};
 /**
  * Возвращает случайное число в заданном диапазоне.
  * @param {number} min - минимальное значение
@@ -93,27 +97,50 @@ var getRandomProperty = function (obj) {
 };
 /**
 * Возращает объект.
+* @param {number} numberImg - номер изображения
+* @param {string} housingTitle - заголовок объявления
+* @param {number} price - число, случайная цена
+* @param {string} type - строка с одним из четырёх фиксированных значений
+* @param {number} rooms - число, случайное количество комнат
+* @param {number} guests - число, случайное количество гостей
+* @param {string} checkin - строка с одним из трёх фиксированных значений
+* @param {string} checkout - строка с одним из трёх фиксированных значений
+* @param {Array} features - массив строк случайной длины
+* @param {string} discription - пустая строка
+* @param {Array} photos - массив из строк
 * @return {Object} объект объявления
 */
-var generateObject = function () {
+var generateObject = function (
+    numberImg,
+    housingTitle,
+    price,
+    type,
+    rooms,
+    guests,
+    checkin,
+    checkout,
+    features,
+    discription,
+    photos
+) {
   var x = getRandomInt(MIN_X, MAX_X);
   var y = getRandomInt(MIN_Y, MAX_Y);
   var pinObj = {
     'author': {
-      'avatar': 'img/avatars/user01.png'
+      'avatar': 'img/avatars/user0' + numberImg + '.png',
     },
     'offer': {
-      'title': getRandomElementFromArray(TITLE),
-      'addres': x + ', ' + y,
-      'price': getRandomInt(MIN_PRICE, MAX_PRICE),
-      'type': getRandomProperty(AccomodationType),
-      'rooms': getRandomInt(MIN_ROOMS, MAX_ROOMS),
-      'guests': getRandomInt(MIN_GUESTS, MAX_GUESTS),
-      'checkin': getRandomElementFromArray(CHECKIN),
-      'checkout': getRandomElementFromArray(CHECKOUT),
-      'features': getRandomLengthOfArray(FEATURES),
-      'discription': '',
-      'photos': PHOTOS
+      'title': housingTitle,
+      'address': x + ', ' + y,
+      'price': price,
+      'type': type,
+      'rooms': rooms,
+      'guests': guests,
+      'checkin': checkin,
+      'checkout': checkout,
+      'features': features,
+      'discription': discription,
+      'photos': photos
     },
     'location': {
       'x': x,
@@ -122,7 +149,6 @@ var generateObject = function () {
   };
   return pinObj;
 };
-
 /**
 * Возращает массив объектов.
 * @param {number} countAdverts - количество объявлений
@@ -131,7 +157,19 @@ var generateObject = function () {
 var generateArrayOfAdverts = function (countAdverts) {
   var advertsArray = [];
   for (var i = 1; i <= countAdverts; i++) {
-    advertsArray.push(generateObject());
+    advertsArray.push(generateObject(
+        i,
+        getRandomElementFromArray(TITLE),
+        getRandomInt(MIN_PRICE, MAX_PRICE),
+        getRandomProperty(AccomodationType),
+        getRandomInt(MIN_ROOMS, MAX_ROOMS),
+        getRandomInt(MIN_GUESTS, MAX_GUESTS),
+        getRandomElementFromArray(CHECKIN),
+        getRandomElementFromArray(CHECKOUT),
+        getRandomLengthOfArray(FEATURES),
+        '',
+        PHOTOS
+    ));
   }
   return advertsArray;
 };
@@ -148,9 +186,11 @@ var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pi
 */
 var renderPin = function (pin) {
   var pinElement = pinTemplate.cloneNode(true);
-  pinElement.querySelector('img').src = 'img/avatars/user0' + i + '.png';
+  pinElement.querySelector('img').src = pin.author.avatar;
   pinElement.querySelector('img').alt = pin.offer.title;
-  pinElement.style = 'left: ' + pin.location.x + 'px; top: ' + pin.location.y + 'px;';
+  pinElement.style =
+  'left: ' + (pin.location.x - MyPin.WIDTH / 2) +
+  'px; top: ' + (pin.location.y - MyPin.HEIGHT) + 'px;';
   return pinElement;
 };
 /**
@@ -159,8 +199,21 @@ var renderPin = function (pin) {
 var renderPinsOnMap = function () {
   var fragment = document.createDocumentFragment();
   for (var i = 1; i <= COUNT_ADVERTS; i++) {
-    fragment.appendChild(renderPin(generateObject()));
+    fragment.appendChild(renderPin(generateObject(
+        i,
+        getRandomElementFromArray(TITLE),
+        getRandomInt(MIN_PRICE, MAX_PRICE),
+        getRandomProperty(AccomodationType),
+        getRandomInt(MIN_ROOMS, MAX_ROOMS),
+        getRandomInt(MIN_GUESTS, MAX_GUESTS),
+        getRandomElementFromArray(CHECKIN),
+        getRandomElementFromArray(CHECKOUT),
+        getRandomLengthOfArray(FEATURES),
+        '',
+        PHOTOS)));
   }
   mapPins.appendChild(fragment);
 };
 renderPinsOnMap();
+
+// пункт 4
